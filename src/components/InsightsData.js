cbcda3822b98425f6f0283e94597773c24a2ce1a
Sintu Kumar <sintu9155624748@gmail.com>
2026-08-01 16:01:28 +0530

    bugs fix in Insights

--- src/components/InsightsData.js
+++ src/components/InsightsData.js
@@ -3,17 +3,17 @@ export const insightsMenu = {
     {
       title: "Library",
       desc: "E-books, articles, and podcasts with data, stories, and news about business and innovation.",
-      link:""
+      link: "/library",
     },
     {
       title: "Case Studies",
       desc: "Discover digital transformation stories co-created with leading companies.",
-      link:""
+      link: "/case-studies",
     },
     {
       title: "Newsroom",
       desc: "Press releases, Stefanini in the media, materials, and specific points of contact.",
-      link:""
+      link: "/newsroom",
     },
   ],
 };
